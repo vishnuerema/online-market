4866f462dab8ac4e2ad22c0743f37f3469fe076c
vishnuerema <007vishnue@gmail.com>
2020-02-18 16:49:59 +0530

    feb18rt

--- src/Pages/View-product.js
+++ src/Pages/View-product.js
@@ -67,13 +67,25 @@ constructor(props){
                         <div className="viewprod_link"><Link  to='/'  className='productview_a'>Home</Link></div>
                         <div className="head_name_prod">{val.home_product_name}</div>
                         <div className="price_name_prod"><span className="cost_price">₹{val.home_product_amount.cost}</span><span className="discount_price"><strike>₹{val.home_product_amount.discount}</strike></span><span className="actul_price">₹{val.home_product_amount.actual_price}</span></div>
-                        <div className="color_name_prod"> {val.home_product_attributes.map((datas,index) => (<span key={index} className="color_prod">{datas.color}</span> ))}</div>
+                        <div className="color_name_prod_head">Color : </div>
+                        <div className="color_name_prod"> 
+                        {val.home_product_attributes.map((colors, index) => (
+                            <div key={index} className="Viewproduct_color">
+                              {colors.color.map((color, color_i)=> (<span key={color_i} style={{background: color}} className="color_prod rounded-circle"></span>))}
+                                
+                            </div>
+                          ))}
+                          </div>
                         <div className="view_prod_highlt">
                         <p className="viewprod_highlit_head">Highlights</p>
                         <div className="view_prod_highlt_list"><span className="viewprod_highlit_head">Size</span> : <span>{val.home_product_specification.size.replace(/<[^>]*>?/gm, '')}</span></div>
                         <div className="view_prod_highlt_list"><span className="viewprod_highlit_head">Color</span> : <span>{val.home_product_specification.color.replace(/<[^>]*>?/gm, '')}</span></div>
                         <div className="view_prod_highlt_list"><span className="viewprod_highlit_head">Proof</span> : <span>{val.home_product_specification.proof.replace(/<[^>]*>?/gm, '')}</span></div>
                         <div className="view_prod_highlt_list"><span className="viewprod_highlit_head">Weight</span> : <span>{val.home_product_specification.weight.replace(/<[^>]*>?/gm, '')}</span></div>
+                        <div className="view_prod_highlt_list"><span className="viewprod_highlit_head">Form factor</span> : <span>{val.home_product_specification['form factor'].replace(/<[^>]*>?/gm, '')}</span></div>
+                        <div className="view_prod_highlt_list"><span className="viewprod_highlit_head">Box includes</span> : <span>{val.home_product_specification['box includes'].replace(/<[^>]*>?/gm, '')}</span></div>
+                        <div className="view_prod_highlt_list"><span className="viewprod_highlit_head">Material type</span> : <span>{val.home_product_specification['material type'].replace(/<[^>]*>?/gm, '')}</span></div>
+                        <div className="view_prod_highlt_list"><span className="viewprod_highlit_head">manufacturer warrent</span> : <span>{val.home_product_specification['manufacturer warrent'].replace(/<[^>]*>?/gm, '')}</span></div>
                       </div>
                       </div>
                   </div>
